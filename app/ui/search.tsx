e16a2ccb9641b95ec969853679d7d6cc60b33372
Jiramate Kedmake <jiramate2548@gmail.com>
2025-03-12 11:26:31 +0700

    Fix Search arguments

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -4,11 +4,7 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, useRouter, usePathname } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
-
-
-
-
-export default function Search(placeholder: string) {
+export default function Search({ placeholder }: { placeholder: string }) {
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const { replace } = useRouter();
